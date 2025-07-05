947f794f6e85db3849df3b706b3c011d5b2d5da1
Dankházi Gergely <dankhazi.gergely@gmail.com>
2025-07-05 22:49:54 +0200

    expoprt fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',  // Enable static export
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
